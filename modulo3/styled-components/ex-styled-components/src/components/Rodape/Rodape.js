654d24ddebbe90794fedba9dc58f0b50cce63cbc
Bianca Paccola <bianca.paccola@gmail.com>
2022-06-30 21:12:43 -0300

    Add exerc LabZap

---
+++ modulo3/styled-components/ex-styled-components/src/components/Rodape/Rodape.js
@@ -0,0 +1,15 @@
+import react from 'react'
+import App from '../../App'
+import {Footer} from '../../style'
+import logolabenu from '../images/labenu-logo.jpg'
+
+export function Rodape() {
+    return (
+        <div>
+            <Footer>
+                <p>Copryght &copy; Labenu All rights reserved. R. Pais Leme, 215, Conjunto 820 Pinheiros. CEP: 05424-150
+                </p>
+            </Footer>
+        </div>
+    )
+}
