889e47641727c344653c68c89f01f601f75eac9f
Bianca Paccola <bianca.paccola@gmail.com>
2022-07-05 18:32:36 -0300

    lista de objetos

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -4,23 +4,34 @@ import './style.css'
 
 
 function App() {
+
+  const postagem = [
+    {
+    nomeUsuario:'paulinha',
+    fotoUsuario:'https://picsum.photos/50/50',
+    fotoPost:'https://picsum.photos/200/150'
+    },
+
+    {
+      nomeUsuario:'aninha',
+      fotoUsuario:'https://picsum.photos/id/239/50/50',
+      fotoPost:'https://picsum.photos/id/237/200/150'
+    },
+
+    {
+      nomeUsuario:'juninho',
+      fotoUsuario:'https://picsum.photos/id/238/50/50',
+      fotoPost: 'https://picsum.photos/id/236/200/150'
+    }
+  ]
+
 return(
   <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-                     <Post
-            nomeUsuario={'aninha'}
-            fotoUsuario={'https://picsum.photos/id/239/50/50'}
-            fotoPost={'https://picsum.photos/id/237/200/150'}
-          />
-                     <Post
-            nomeUsuario={'juninho'}
-            fotoUsuario={'https://picsum.photos/id/238/50/50'}
-            fotoPost={'https://picsum.photos/id/236/200/150'}
-          />
+    {
+      postagem.map((item)=> {
+      return <Post nomeUsuario={item.nomeUsuario} fotoUsuario={item.fotoUsuario} fotoPost={item.fotoPost} />
+    })
+   }
   </div>
 )
 
