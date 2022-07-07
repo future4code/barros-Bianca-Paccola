c271842d3795e79e3ce533facae1f67d2d6f4777
Bianca Paccola <bianca.paccola@gmail.com>
2022-07-07 18:32:21 -0300

    add inputs

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Post/Post';
 import './style.js'
-import {MainContainer} from './style.js'
+import {MainContainer, Form} from './style.js'
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
@@ -22,7 +22,13 @@ font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
 
 function App() {
 
-  const postagem = [
+  //seta o estado inicial dos inputs do form
+  const [inputNome, setInputNome] = useState('')
+  const [inputFotoPerfil, setInputFotoPerfil] = useState('')
+  const [inputFotoPost, setInputFotoPost] = useState('')
+
+  //seta o estado inicial do array que vai gerar as postagens
+  const [feed, setfeed] = useState([
     {
     nomeUsuario:'paulinha',
     fotoUsuario:'https://picsum.photos/50/50',
@@ -40,19 +46,58 @@ function App() {
       fotoUsuario:'https://picsum.photos/id/238/50/50',
       fotoPost: 'https://picsum.photos/id/236/200/150'
     }
-  ]
+  ])
 
-  const postagensMap = postagem.map((item)=> {
+  //pega o que é preenchido no input de Nome
+  const preencheNome = (event) => {
+    setInputNome(event.target.value)
+  }
+
+  //pega o que é preenchido no input de Foto
+  const preencheFotoPerfil = (event) => {
+    setInputFotoPerfil(event.target.value)
+  }
+
+  //pega o que é preenchido no segundo input de Foto
+  const preencheFotoPost = (event) => {
+    setInputFotoPost(event.target.value)
+  }
+
+  //Evento de clique
+  const enviaPost = (event) => {
+    
+    //Previne que o envio do formulário recarregue a página
+    event.preventDefault()
+
+    //Cria um novo array, espelhado no primeiro e adiciona mais um objeto com os dados que foram captados nos inputs
+    const feedAtualizado = [...feed, {nomeUsuario: inputNome, fotoUsuario: inputFotoPerfil, fotoPost: inputFotoPost}]
+
+    //Atualiza o array original com os novos dados
+    setfeed(feedAtualizado)
+
+    //Retorna o input ao estado inicial vazio, pronto para receber novos dados
+    setInputNome('')
+    setInputFotoPerfil('')
+    setInputFotoPost('')
+  }
+
+  //Mapeia os objetos do array para poder rederiza-los depois na tela
+  const feedMap = feed.map((item)=> {
     return <Post nomeUsuario={item.nomeUsuario} fotoUsuario={item.fotoUsuario} fotoPost={item.fotoPost} />})
 
 return(
   <MainContainer>
     <GlobalStyle/>
-      {postagensMap}
+      {feedMap}
+      <Form>
+        <input type='text' required placeholder='Nome de Usuário...' value={inputNome} onChange={preencheNome}></input>
+        <input type='url' required placeholder='Url foto de perfil...' value={inputFotoPerfil}onChange={preencheFotoPerfil}></input>
+        <input type='url' required placeholder='Url foto do post...' value={inputFotoPost} onChange={preencheFotoPost}></input>
+        <button onClick={enviaPost}>Enviar</button>
+      </Form>
   </MainContainer>
 )
 
 }
-
 
 export default App;
