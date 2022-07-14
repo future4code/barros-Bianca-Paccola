a3bdbe3112bf1f428f69d5b13571594c00f5cc84
Bianca Paccola <bianca.paccola@gmail.com>
2022-07-14 19:01:04 -0300

    exerc pensando em react

---
+++ modulo3/thinking-react/MockDeDados.js
@@ -0,0 +1,44 @@
+const listaDeProdutos = [
+    {
+        id: 1,
+        name: "Livro: Horizonte vertical: Onde o fim é apenas o recomeço",
+        price: 19.90,
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/51znDC8+H1L._SY344_BO1,204,203,200_.jpg'
+    },
+
+    {
+        id: 2,
+        name: "Livro: Como Ler Livros",
+        price: 89.09,
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/51Xkaa-973L._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+    },
+
+    {
+        id: 3,
+        name: "Livro: O Mito do Eterno Retorno",
+        price: 54.71,
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41go3ZmbiAL._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+    },
+
+    {
+        id: 4,
+        name: "Livro: Laranja Mecânica",
+        price: 27.46,
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/51cFreMs86L._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+    },
+
+    {
+        id: 5,
+        name: "Livro: A maquina de Uriel",
+        price: 119.99,
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/51n2ln11j-L._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+    },
+
+    {
+        id: 6,
+        name: "Livro: Rumi - A dança da alma",
+        price: 56.62,
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41LHW1AJmWL._SX331_BO1,204,203,200_.jpg'
+    },
+
+]
---
+++ modulo3/thinking-react/README.md
@@ -0,0 +1,11 @@
+# Thinking in React
+
+![arvore](./Capturar.PNG)
+
+# States
+
+![state](./Capturar2.PNG)
+
+# Kanban
+
+https://trello.com/b/hGuEgFDr/projeto-de-fixa%C3%A7%C3%A3o-labecommerce
