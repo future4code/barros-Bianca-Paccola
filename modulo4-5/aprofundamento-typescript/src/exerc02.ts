8ecb37003d61cbb9256198b35ef51ec1c7bc9a50
Bianca Paccola <bianca.paccola@gmail.com>
2022-10-19 21:02:08 -0300

    exercicios do 1 ao 5

--- modulo4-5/aprofundamento-typescript/src/exerc02.ts
+++ modulo4-5/aprofundamento-typescript/src/exerc02.ts
@@ -1,1 +1,41 @@
-console.log("exerc02");
+type Estatisticas = {
+    maior:number,
+    menor:number,
+    media:number
+}
+
+function obterEstatisticas(numeros: number[]): Estatisticas{
+
+    const numerosOrdenados = numeros.sort(
+        (a, b) => a - b
+    )
+
+    let soma: number = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas: Estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+
+console.log(obterEstatisticas([5,10,15,20]));
+
+//
+type Amostra = {
+    numeros: number[],
+    funcao: (numeros: number[]) => Estatisticas;
+}
+
+const amostraDeIdades: Amostra = {
+    numeros: [20, 15, 25, 32, 54, 19],
+    funcao: obterEstatisticas
+}
+
+console.log(amostraDeIdades.funcao(amostraDeIdades.numeros))
--- modulo4-5/aprofundamento-typescript/src/exerc04.ts
+++ modulo4-5/aprofundamento-typescript/src/exerc04.ts
@@ -1,1 +1,37 @@
-console.log("exerc04");
+// a) Crie um arquivo chamado `exercicio-4.ts` , cole o código abaixo e use comentários para responder as questões.
+// b) Como você faria, já com a extensão instalada, para transpilar(converter) esse arquivo typescript em um arquivo javascript?
+//Resoposta: usando o comando tsc, ou colocando direto na área de scipts do package.json: "tsc && node ./caminho"
+
+// `exercicio-4.ts`
+
+// ```tsx
+// type pokemon = {
+// 	name: string,
+//   types: string,
+// 	healthPoints: number
+// }
+
+// const pokemon1: pokemon = {
+//   name: "Charmander",
+//   types: "Fire",
+//   healthPoints: 28
+// }
+
+// const pokemon2: pokemon = {
+//   name: "Bulbasaur",
+//   types: "Grass/Poison",
+//   healthPoints: 31
+// }
+
+// const pokemon3: pokemon = {
+//   name: "Squirtle",
+//   types: "Water",
+//   healthPoints: 35
+// }
+// ```
+
+// c) E se este arquivo estivesse dentro de uma pasta chamada `src`. O processo seria diferente? Se sim, descreva as diferenças.
+//Resposta: Sim. Precisa espeficiar o caminho na propriedade rootDir do tsconfig.json
+
+// d) Existe alguma maneira de **transpilar** múltilplos arquivos de uma vez só? Caso conheça, explique como fazer.
+// Resposta: Existem 2 maneiras. Pode-se usar "tsc ./caminho" e mandar mais arquivos seperados por espaço, ou usar apenas o comando tsc sozinho que ele vai transpilar todos os arquivos .ts que encontrar no projeto.
