0d50579b4419fbba87b01435266c7794585bbaff
Bianca Paccola <bianca.paccola@gmail.com>
2022-10-21 20:45:46 -0300

    exercicios 01 e 02

--- modulo4-5/lista-typescript/src/exerc01.ts
+++ modulo4-5/lista-typescript/src/exerc01.ts
@@ -1,1 +1,12 @@
-console.log("exercício 01");
+// Crie um função que receba uma string com o nome e outra string com uma data de nascimento (ex.: “24/04/1993”). A função deve separar o dia, o mês e ano e retornar uma string no seguinte formato: "Olá me chamo {NOME}, nasci no dia {DIA} do mês de {MÊS} do ano de {ANO}" 
+
+const info = require('readline-sync');
+
+function apresentation (): string {
+   const name = info.question("Qual seu nome? ")
+   const birthDate = info.question("Qual sua data de nascimento? ").replaceAll("-", "/").split("/")
+
+   return `Olá! Me chamo ${name}, nasci no dia ${birthDate[0]} do mês de ${birthDate[1]} do ano de ${birthDate[2]}.`
+}
+
+console.log(apresentation());
--- modulo4-5/lista-typescript/src/exerc02.ts
+++ modulo4-5/lista-typescript/src/exerc02.ts
@@ -1,1 +1,9 @@
-console.log("exercício 02");
+//Crie uma função que receba um parâmetro qualquer e que imprima no console o tipo da variável. 
+
+function typeOfVariable (element: any) : void {
+    console.log(typeof element);
+}
+
+typeOfVariable(32);
+typeOfVariable("string");
+typeOfVariable(true);
