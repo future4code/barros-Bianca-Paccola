18e655aa2f69633448102c3d65dbb489f0666f51
Bianca Paccola <bianca.paccola@gmail.com>
2022-10-23 20:42:56 -0300

    exercicios do 03 ao 06

--- modulo4-5/lista-typescript/src/exerc05.ts
+++ modulo4-5/lista-typescript/src/exerc05.ts
@@ -1,1 +1,22 @@
-console.log("exercício 05");
+//Considerando o array de usuários abaixo, crie uma função que receba este array como parâmetro e retorne uma lista com apenas os emails dos usuários “admin”. 
+
+type User = {
+    name: string,
+    email: string,
+    role:  string
+}
+
+let users = [
+	{name: "Rogério", email: "roger@email.com", role: "user"},
+	{name: "Ademir", email: "ademir@email.com", role: "admin"},
+	{name: "Aline", email: "aline@email.com", role: "user"},
+	{name: "Jéssica", email: "jessica@email.com", role: "user"},  
+	{name: "Adilson", email: "adilson@email.com", role: "user"},  
+	{name: "Carina", email: "carina@email.com", role: "admin"}      
+] 
+
+function getAdminsEmails(array: User[]): string[] {
+return array.filter((user) => user.role === "admin").map((user) => user.email)
+}
+
+console.log(getAdminsEmails(users));
--- modulo4-5/lista-typescript/src/exerc06.ts
+++ modulo4-5/lista-typescript/src/exerc06.ts
@@ -1,1 +1,28 @@
-console.log("exercício 06");
+//Criar uma função que receba este array como parâmetro, atualize o saldo total descontando todos os débitos e retorne apenas os clientes com saldo negativo.
+
+type Client = {
+    client: string;
+    totalBalance: number;
+    debits : number[];
+}
+
+let clients = [
+	{ client: "João", totalBalance: 1000, debits: [100, 200, 300] },
+	{ client: "Paula", totalBalance: 7500, debits: [200, 1040] },
+	{ client: "Pedro", totalBalance: 10000, debits: [5140, 6100, 100, 2000] },
+	{ client: "Luciano", totalBalance: 100, debits: [100, 200, 1700] },
+	{ client: "Artur", totalBalance: 1800, debits: [200, 300] },
+	{ client: "Soter", totalBalance: 1200, debits: [] }
+]
+
+function getDebitAccount (array: Client[]) : Client[] {
+    const sumDebbits = array.map((account)=>{
+        return {client: account.client, totalBalance: account.totalBalance - account.debits.reduce((value1, value2)=> {
+            return value1 + value2
+        },0), debits: []}
+    }).filter((account)=> account.totalBalance < 0);
+
+    return sumDebbits;
+}
+
+console.log(getDebitAccount(clients));
