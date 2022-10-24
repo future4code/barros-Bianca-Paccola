69a7569918a02a3a1d37f5fe6ccbc310cfe3defb
Bianca Paccola <bianca.paccola@gmail.com>
2022-10-23 22:17:00 -0300

    exercicio 08

--- modulo4-5/lista-typescript/src/exerc08.ts
+++ modulo4-5/lista-typescript/src/exerc08.ts
@@ -1,1 +1,14 @@
-console.log("exercício 08");
+//A quantidade de anagramas de uma palavra sem nenhuma letra repetida é o fatorial da quantidade de letras. Para mesa, a quantidade é 4! = 24
+//Escreva uma função que receba uma palavra (sem letras repetidas) e devolva a quantidade de anagramas que ela possui.
+
+function getFactorial (word: string) : number {
+
+    if (word.length === 1) {
+        return 1;
+    }
+    
+    console.log(word.length)
+    return word.length * getFactorial(word.substring(1)) //Recursion
+}
+
+console.log(getFactorial("word"));
