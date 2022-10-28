7e1059b8f7953e7389af4e37df733643cabfc91a
Bianca Paccola <bianca.paccola@gmail.com>
2022-10-28 18:44:12 -0300

    add instalação e primeiro teste

---
+++ modulo4-5/aprofundamento-express/src/index.ts
@@ -0,0 +1,16 @@
+import express, {Response, Request} from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/ping", (req: Request, res: Response) => {
+    res.status(200).send("Pong! 🏓")
+})
+
+
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+})
