43e03a6e3306f88bbc4f1ba83651154a880fb320
Bianca Paccola <bianca.paccola@gmail.com>
2022-10-28 23:29:06 -0300

    exercícios do 2 ao 7

--- modulo4-5/aprofundamento-express/src/index.ts
+++ modulo4-5/aprofundamento-express/src/index.ts
@@ -1,14 +1,70 @@
 import express, {Response, Request} from 'express';
 import cors from 'cors';
+import {Todo} from './types'
+import { toDos } from './data';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Test
 app.get("/ping", (req: Request, res: Response) => {
     res.status(200).send("Pong! 🏓")
-})
+});
 
+//Get To do List with status true or false
+app.get("/todos", (req: Request, res: Response) => {
+    const toDoStatus = req.body.completed
+    const toDoListStatus = toDos.filter((todo)=> todo.completed === toDoStatus);
+    res.status(200).send(toDoListStatus);
+});
+
+//Add to do list
+// Request => headers: authorization = number, body = "titles": ["Levar o dog passear", "Fazer almoço", "Pagar contas"]
+app.put("/todo/add", (req: Request, res: Response) => {
+    const userId = Number(req.headers.authorization)
+    const body = req.body.titles
+    const id = toDos[toDos.length -1].id
+    body.forEach((item: string, index: number) => {
+        return toDos.push({
+            userId: userId, 
+            id: id+index+1, 
+            title: item, 
+            completed: false
+        })
+    })
+    res.status(200).send(toDos);
+});
+
+//Change the status
+app.put("/todo/:id",  (req: Request, res: Response) => {
+    const itemId = Number(req.params.id)
+    const itemStatus = req.body.completed
+    const itemIndex = toDos.findIndex((item) => item.id === itemId);
+    toDos[itemIndex].completed = itemStatus;
+
+    res.status(200).send(toDos[itemIndex]);
+});
+
+//Delete item
+app.delete("/todo/:id",  (req: Request, res: Response) => {
+    const itemId = Number(req.params.id)
+    const itemIndex = toDos.findIndex((item) => item.id === itemId);
+    toDos.splice(itemIndex,1)
+
+    res.status(200).send(toDos);
+});
+
+//Build an to do list
+// Request => headers: authorization = number, body = "titles": ["Levar o dog passear", "Fazer almoço", "Pagar contas"]
+app.post("/todo/post", (req: Request, res: Response) => {
+    const userId = Number(req.headers.authorization)
+    const body = req.body.titles
+    const toDoList = body.map((item: string, index: number) => {
+        return {userId: userId, id: index, title: item, completed: false}
+    })
+    res.status(200).send(toDoList);
+});
 
 
 app.listen(3003, () => {
