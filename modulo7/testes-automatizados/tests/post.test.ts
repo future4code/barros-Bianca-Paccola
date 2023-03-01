1d2759846c22d536a7e00f24bea484a937b29f68
Bianca Paccola <bianca.paccola@gmail.com>
2023-03-01 05:13:16 -0300

    arrumar afterAll

--- modulo7/testes-automatizados/tests/post.test.ts
+++ modulo7/testes-automatizados/tests/post.test.ts
@@ -17,23 +17,24 @@ abstract class TestPostRemove extends BaseDatabase {
 
 describe("Testes para inserir posts na tabela labook_posts do banco de dados", () => {
   const postDatabase = new PostDatabase();
-
+  let id:string;
   test("inserir um post na tabela", async () => {
-    const newPost: postInputDTO = {
-      photo:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dicio.com.br%2Fpost%2F&psig=AOvVaw1De234mSwUxojBnYt9LG3l&ust=1677729095976000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIjMxIjquf0CFQAAAAAdAAAAABAE",
-      description: "Este é um post!",
-      type: POST_TYPES.NORMAL,
-      authorId: "23e09197-0432-4d38-a3d0-41614038924a",
-    };
-
-    const idPost: string = await createPost(newPost);
-    const result = await postDatabase.getPostById(idPost);
+      const newPost: postInputDTO = {
+          photo:
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dicio.com.br%2Fpost%2F&psig=AOvVaw1De234mSwUxojBnYt9LG3l&ust=1677729095976000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIjMxIjquf0CFQAAAAAdAAAAABAE",
+          description: "Este é um post!",
+          type: POST_TYPES.NORMAL,
+          authorId: "23e09197-0432-4d38-a3d0-41614038924a",
+        };
+        
+    const post: string = await createPost(newPost);
+    id = post
+    const result = await postDatabase.getPostById(id);
 
     expect(result).toBeDefined()
-    expect(result[0].id).toEqual(idPost)
+    expect(result[0].id).toEqual(id)
+});
     afterAll(async () => {
-      await TestPostRemove.remove(idPost);
+    await TestPostRemove.remove(id);
     });
-  });
 });
