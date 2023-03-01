30854cb958b07e0b6fd8409e18ace2e8de3953b5
Bianca Paccola <bianca.paccola@gmail.com>
2023-02-28 22:32:37 -0300

    refatoração dos testes

--- modulo7/testes-automatizados/tests/user.test.ts
+++ modulo7/testes-automatizados/tests/user.test.ts
@@ -2,32 +2,43 @@ import { userPurchase } from "../src/functions"
 import { userDto, userInputPurchaseDTO } from "../src/model/userDTO"
 
 describe("Testes do usuário", () => {
-    const userPurchaseTest:userInputPurchaseDTO = {
-        user: {
-            name: "Bianca Paccola",
-            userBalance: 550
-        },
-        value: 550
-    }
-    const responsePurchase:userDto = {
-        name: userPurchaseTest.user.name,
-        userBalance: userPurchaseTest.user.userBalance - userPurchaseTest.value
-    }
 
     test("Teste de compra do cliente", () => {
-        expect(userPurchase(userPurchaseTest)).toStrictEqual(responsePurchase)
+        expect(userPurchase({
+            user: {
+                name: "Bianca Paccola",
+                userBalance: 550
+            },
+            value: 550
+        })).toStrictEqual({
+            name: "Bianca Paccola",
+            userBalance: 0
+        })
+    })
+
+    test("Teste de compra do cliente", () => {
+        expect(userPurchase({
+            user: {
+                name: "Bianca Paccola",
+                userBalance: 1000
+            },
+            value: 550
+        })).toStrictEqual({
+            name: "Bianca Paccola",
+            userBalance: 450
+        })
     })
 })
 
 describe("Teste de erros do usuário", () => {
-    const userPurchaseTest:userInputPurchaseDTO = {
-        user: {
-            name: "Bianca Paccola",
-            userBalance: 500
-        },
-        value: 550
-    }
+
     test("Testando erro caso o saldo seja insuficiente para a compra", () => {
-        expect(userPurchase(userPurchaseTest)).toBe(undefined)
+        expect(userPurchase({
+            user: {
+                name: "Bianca Paccola",
+                userBalance: 500
+            },
+            value: 550
+        })).toBe(undefined)
     })
 })
