ca834ff92a35744f72c4665b406978f214274cde
Bianca Paccola <bianca.paccola@gmail.com>
2023-03-03 01:23:06 -0300

    exercicio 03

--- modulo7/testes-unitarios-mocks/src/functionsCharacter.ts
+++ modulo7/testes-unitarios-mocks/src/functionsCharacter.ts
@@ -1,6 +1,6 @@
 import { characterDTO } from "./model/characterDTO";
 
-export const validateCharacter = (character: characterDTO) => {
+export const validateCharacter = (character: characterDTO):boolean => {
     if (!character.name || !character.life || !character.defense || !character.strength ) {
         return false;
     } 
@@ -9,4 +9,17 @@ export const validateCharacter = (character: characterDTO) => {
     }
     
     return true;
+}
+
+export const performAttack = (
+    attacker: characterDTO, 
+    defender: characterDTO, 
+    validator: (input: characterDTO) => boolean
+    ) => {
+    if (!validator(attacker) || !validator(defender)) {
+        throw new Error("Invalid character")
+    }
+    if (attacker.strength > defender.defense) {
+        defender.life -= attacker.strength - defender.defense
+    }
 }
