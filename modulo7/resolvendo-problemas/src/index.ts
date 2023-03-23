0c37af87d6bf7db6d8953bd30a881e153d84e36e
Bianca Paccola <bianca.paccola@gmail.com>
2023-03-23 18:04:11 -0300

    exercício 04, a e b

--- modulo7/resolvendo-problemas/src/index.ts
+++ modulo7/resolvendo-problemas/src/index.ts
@@ -4,7 +4,7 @@
     Considere que a gente só possa fazer três operações com `string`: adicionar um caractere ao final dele, remover um caractere do final dele ou substituir um caractere por outro. Dizemos que uma `string` é 'one edit' de outra se ela for o resultado da original a partir de UMA SÓ dessas operações. Escreva um método que determine se uma `string` é  'one edit' de outra.
 */
 
-function isOneEdit(strA: string, strB: string): boolean {
+const isOneEdit = (strA: string, strB: string): boolean =>  {
   // Se a diferença de tamanho entre as duas é maior que 1, não é oneEdit
   if (Math.abs(strB.length - strA.length) > 1) return false
 
@@ -21,10 +21,10 @@ function isOneEdit(strA: string, strB: string): boolean {
   // Retorna true se a diferença é 1, false caso contrário
   return charsDiffCount === 1
 }
-console.log(isOneEdit('banan', 'banana')) //true
-console.log(isOneEdit('bananak', 'banana')) //true
-console.log(isOneEdit('panana', 'banana')) //true
-console.log(isOneEdit('bananaaa', 'banana')) //false
+// console.log(isOneEdit('banan', 'banana')) //true
+// console.log(isOneEdit('bananak', 'banana')) //true
+// console.log(isOneEdit('panana', 'banana')) //true
+// console.log(isOneEdit('bananaaa', 'banana')) //false
 
 
 /*
@@ -48,11 +48,11 @@ const compress = (str: string):string => {
   return result.length > str.length ? str : result;
 }
 // Testes
-console.log(compress('aabbb')) // a2b3
-console.log(compress('aabcccccaaa')) // a2b1c5a3
-console.log(compress('accurate')) // accurate
-console.log(compress('escola')) // escola
-console.log(compress('accuraaaaaaaaaate')) // a1c2u1r1a10t1e1
+// console.log(compress('aabbb')) // a2b3
+// console.log(compress('aabcccccaaa')) // a2b1c5a3
+// console.log(compress('accurate')) // accurate
+// console.log(compress('escola')) // escola
+// console.log(compress('accuraaaaaaaaaate')) // a1c2u1r1a10t1e1
 
 
 // ------------ Fazer a compressão sem ser em sequência --------------- //
@@ -75,7 +75,7 @@ const stringCompress = (str: string): string => {
     return aux;
 }
 
-console.log(stringCompress("aabcccccaaa"))
+// console.log(stringCompress("aabcccccaaa"))
 
 /*
 - Exercício 3
@@ -95,12 +95,23 @@ console.log(stringCompress("aabcccccaaa"))
     b. Escreva uma função que receba uma Matriz, o índice da linha, o índice da coluna e um valor. Essa função deve alterar o elemento das linha e coluna em questão pelo valor passado. (Caso não tenha nenhum valor nos índices passados, retorne um erro: `Fora do intervalo da matriz`)
 */
 
-let matrix = new Array(3)
-matrix[0] = [5,7,8]
-matrix[1] = [6,2,1]
-matrix[2] = [3,9,4]
+let matrix1 = new Array(3)
+matrix1[0] = [5,7,8]
+matrix1[1] = [6,2,1]
+matrix1[2] = [3,9,4]
 
-function replaceMatrixValue (matrix: number[][], indexRow: number, indexCol: number, value:number):void {
+let matrix2 = new Array(3)
+matrix2[0] = [3,2,1]
+matrix2[1] = [5,4,6]
+matrix2[2] = [9,7,8]
+
+/*
+8, 9, 9
+11, 6, 7,
+12, 16, 12
+*/
+
+const replaceMatrixValue = (matrix: number[][], indexRow: number, indexCol: number, value:number):void => {
   if(!matrix[indexRow] || !matrix[indexRow][indexCol]) {
     throw console.error("\x1b[31mError: Out of array range");
   } else {
@@ -109,5 +120,31 @@ function replaceMatrixValue (matrix: number[][], indexRow: number, indexCol: num
   
   console.table(matrix);
 }
+// replaceMatrixValue(matrix1, 1, 1, 6);
 
-console.log(replaceMatrixValue(matrix, 1, 1, 6))
+/*
+  a. Implemente uma função que receba uma matriz e imprima, no console, todos os seus elementos
+*/
+const printMatrix = (matrix: number[][]): void => {
+  console.table(matrix)
+}
+// printMatrix(matrix1);
+
+/*
+b. Implemente uma função que receba duas matrizes e devolva a soma delas
+*/
+function sumMatrix(matrix1: number[][], matrix2: number[][]): void {
+
+  let matrixResult:number[][] = [];
+
+  for(let i = 0; i < matrix1.length; i++) {
+    let rows:number[] = []
+    for(let j = 0; j < matrix1[i].length; j++) {
+      rows.push(matrix1[i][j] + matrix2[i][j])
+    }
+    matrixResult.push(rows)
+  }
+
+  console.table(matrixResult);
+}
+// sumMatrix(matrix1, matrix2)
